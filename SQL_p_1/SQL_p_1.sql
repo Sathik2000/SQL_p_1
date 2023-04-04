74e5f5c01ee62b92d38552d74103cbc502cd4650
Sathik2000 <125662651+Sathik2000@users.noreply.github.com>
2023-04-05 00:19:14 +0530

    Add files via upload

---
+++ SQL_p_1/SQL_p_1.sql
@@ -0,0 +1,22 @@
+Create database School_2;
+use school_2;
+create table School_S(
+ID int,
+Name varchar(50),
+Result varchar(50)
+);
+insert into School_S value
+(5128,'Sanga','pass'),
+(5127,'Mahela','pass'),
+(5126,'Sanath','Fail'),
+(5125,'Upul','Fail'),
+(5124,'Dilashan','pass'),
+(5123,'Aththapaththy','pass'),
+(5122,'Kusal','Fail'),
+(5121,'mendis','pass'),
+(5120,'thisara','fail'),
+(5119,'Silva','pass');
+
+
+
+
